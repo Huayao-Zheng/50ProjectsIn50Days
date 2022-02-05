c489f2bab321279cd0d47c902d834d174df098b5
lonelybuddy <henryzheng92@gmail.com>
2022-02-04 20:49:40 -0600

    completed Day 27 Toast notification

--- Day27-Toast-Notification/script.js
+++ Day27-Toast-Notification/script.js
@@ -1,1 +1,23 @@
+const button = document.getElementById('button');
+const toasts = document.getElementById('toasts');
 
+const messages = ['Message One', 'Message Two', 'Message Three', 'Message Four'];
+const types = ['info', 'success', 'error'];
+
+const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
+const randomType = () => types[Math.floor(Math.random() * types.length)];
+
+const createNotification = (message = null, type = null) => {
+    const notification = document.createElement('div');
+
+    notification.classList.add('toast');
+    notification.classList.add(type ? type : randomType());
+
+    notification.innerText = message ? message : randomMessage();
+
+    toasts.appendChild(notification);
+
+    setTimeout(() => notification.remove(), 3000);
+};
+
+button.addEventListener('click', () => createNotification());
